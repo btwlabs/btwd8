84b15d3b2e559bce171bd925cd33574478b7f3f5
Ted Benice <ted@bythewaylabs.com>
2019-10-15 10:46:51 -0700

    remove a bunch of composer managed stuff

--- themes/contrib/basic/js/build/scripts.js
+++
@@ -1,2 +0,0 @@
-"use strict";!function(n,o){n.behaviors.basic={attach:function(n,i){o(window).on("load",function(){}),o(window).on("resize",function(){}),o(window).on("scroll",function(){})}}}(Drupal,jQuery);
-//# sourceMappingURL=scripts.js.map
