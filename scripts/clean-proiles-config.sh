4d6fe04f04341156099c7601da59f6ce6a61a5fc
Ted Benice <ted@bythewaylabs.com>
2020-01-17 15:37:41 -0800

    fix config copy scripts

--- scripts/clean-proiles-config.sh
+++ scripts/clean-proiles-config.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 find $SCRIPT_DIR/../profiles/brochure/config/install/ -type f -exec sed -i '' '/^uuid: /d' {} \;
-find $SCRIPT_DIR/../profiles/brochure/config/install/ -type f -exec sed -i '' '/_core:/{N;d;}' {} \;
+find $SCRIPT_DIR/../profiles/brochure/config/install/ -type f -exec sed -i '' '/^_core:/{N;d;}' {} \;
--- scripts/copy-canonical.sh
+++ scripts/copy-canonical.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-rsync -avz /Users/tbenice/PhpstormProjects/canonical-test/sites/default/config/ /Users/tbenice/PhpstormProjects/d8upstream/profiles/brochure/config/install/
-cd /Users/tbenice/PhpstormProjects/d8upstream
+rsync -avz /Users/tbenice/PhpstormProjects/canonical-test/sites/default/config/ /Users/tbenice/PhpstormProjects/btwd8/profiles/brochure/config/install/
+cd /Users/tbenice/PhpstormProjects/btwd8
 sh scripts/clean-proiles-config.sh
 rm -rf profiles/brochure/config/install/.*.DS_Store
 rm -rf profiles/brochure/config/install/core.extension.yml
@@ -9,3 +9,5 @@ git checkout profiles/brochure/config/install/system.site.yml
 git checkout profiles/brochure/config/install/update.settings.yml
 git checkout profiles/brochure/config/install/views.settings.yml
 git status
+## NOTE LINES ~117-118 of core.entity_view_display.paragraph.gallery.default.yml get overwritten by the cleaning script.
+## Be sure to reset those removals.
