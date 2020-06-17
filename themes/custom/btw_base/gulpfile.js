8f862092c901572a8a8db7d9905a47bc7a58ed0b
Ted <ted@bythewaylabs.com>
2020-06-17 09:54:53 -0700

    update gulp for sass tasks

--- themes/custom/btw_base/gulpfile.js
+++ themes/custom/btw_base/gulpfile.js
@@ -3,51 +3,60 @@
 // ////////////////////////////////////
 
 // Main dependencies
-var gulp 			= require('gulp');
-var browserSync 	= require('browser-sync').create();
-var sass 			= require('gulp-dart-sass');
-var sassGlob 		= require('gulp-sass-glob');
-var autoprefixer 	= require('gulp-autoprefixer');
-var sourcemaps 		= require('gulp-sourcemaps');
-
-
-/**
- * Launch the Server
- */
- gulp.task('browser-sync', ['sass'], function() {
-    browserSync.init({
-		// Change as required, also remember to set in theme settings
-		proxy: "http://canonical-test.lndo.site:8000",
-		//port: 8000
-    });
-});
-
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-dart-sass');
+var sassGlob = require('gulp-sass-glob');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var cache = require('gulp-cache');
 
 /**
  * @task sass
  * Compile files from scss
  */
 gulp.task('sass', function () {
-	return gulp
-		.src('sass/main.scss')
-		.pipe(sassGlob())
-		.pipe(sourcemaps.init())
-		.pipe(sass({includePaths: ['./node_modules/breakpoint-sass/stylesheets']}))
-		.pipe(sass().on('error', sass.logError))
-		.pipe(autoprefixer(['last 3 versions', '> 1%', 'ie 8'], { cascade: true }))
-		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest('css'))
-		.pipe(browserSync.reload({stream:true}))
-});
+    return gulp
+      .src('sass/main.scss')
+      .pipe(sassGlob())
+      .pipe(sourcemaps.init())
+      .pipe(sass({includePaths: ['./node_modules/breakpoint-sass/stylesheets']}))
+      .pipe(sass().on('error', sass.logError))
+      .pipe(autoprefixer(['last 3 versions', '> 1%', 'ie 8'], { cascade: true }))
+      .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest('css'))
+      .pipe(browserSync.reload({stream:true}))
+  }
+
+);
+
+/**
+ * Launch the Server
+ */
+ gulp.task('browser-sync', gulp.series('sass', function() {
+      browserSync.init({
+      // Change as required, also remember to set in theme settings
+      proxy: "https://flat-file-backend.lndo.site"
+      //port: 8000
+      });
+    }
+  )
+ );
 
 
 /**
  * @task reload
  * Refresh the page after clearing cache
  */
-gulp.task('reload', ['clearcache'], function () {
-	browserSync.reload();
-});
+gulp.task('reload', gulp.series(
+    function() {
+      cache.clearAll();
+    },
+    function () {
+      browserSync.reload();
+    }
+  )
+);
 
 
 /**
@@ -56,16 +65,16 @@ gulp.task('reload', ['clearcache'], function () {
  * Clear cache when Drupal related files are changed
  */
 gulp.task('watch', function () {
-	gulp.watch(['sass/*.scss', 'sass/**/*.scss'], ['sass']);
-	gulp.watch(['js/*.js'], ['scripts']);
-	gulp.watch(['templates/*.html.twig', '**/*.yml'], ['reload']);
-});
+    gulp.watch(['sass/*.scss', 'sass/**/*.scss'], gulp.series('sass'));
+    //gulp.watch(['js/*.js'], gulp.series('scripts'));
+    gulp.watch(['templates/*.html.twig', '**/*.yml'],  gulp.series('reload'));
+  }
+);
 
 
 /**
- * Default task, running just `gulp` will 
+ * Default task, running just `gulp` will
  * compile Sass files, launch BrowserSync, watch files.
  */
-gulp.task('default', ['browser-sync', 'watch']);
 
-
+gulp.task('default', gulp.parallel('browser-sync', 'watch'));
